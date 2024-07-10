50339190a33e495096b0b94b21a0ee42d4fd256e
Acliech Chen <2598367363@qq.com>
2024-07-10 11:17:05 +0800

    数据加载

--- util/datasets.py
+++ util/datasets.py
@@ -2,6 +2,9 @@ import os, json, random
 from typing import Any, Optional, Union
 from tqdm import tqdm
 import numpy as np
+import torch
+from torch import Tensor
+from torch.utils.data import Dataset
 
 from .utils import load_wave, wave_to_mels
 
@@ -82,3 +85,76 @@ def split_dataset(
         )
     train_indexes, valid_indexes = indexes[:train], indexes[train : train + valid]
     return train_indexes, valid_indexes
+
+
+class TokMelDataset(Dataset):
+    def __init__(
+        self,
+        meta_infos: list[tuple[str, list[int]]],
+        mel_dir: str,
+        load_from_disk: bool = True,
+    ):
+        super(TokMelDataset, self).__init__()
+        self.dataset: list[tuple[str, list[int]]] = meta_infos
+        self.mel_dir: str = mel_dir
+        self.load_from_disk: bool = load_from_disk
+        if not load_from_disk:
+            self.mel_list: list[Tensor] = [
+                torch.from_numpy(np.load(os.path.join(mel_dir, "{}.npy".format(name))))
+                for name, _ in tqdm(
+                    self.dataset,
+                    desc="Load Mels",
+                    ascii=True,
+                    bar_format="{desc}: {percentage:3.1f}%={n_fmt}/{total_fmt}[{remaining}{postfix}]",
+                )
+            ]
+
+    def __len__(self) -> int:
+        return len(self.dataset)
+
+    def __getitem__(self, idx: int) -> tuple[list[int], Tensor]:
+        name, tokids = self.dataset[idx]
+        if self.load_from_disk:
+            mel = torch.from_numpy(np.load(os.path.join(self.mel_dir, f"{name}.npy")))
+        else:
+            mel = self.mel_list[idx]
+        return tokids, mel
+
+
+class TokMelCollate:
+    def __init__(self):
+        pass
+
+    def __call__(
+        self, batch: list[tuple[list[int], Tensor]]
+    ) -> tuple[Tensor, Tensor, Tensor, Tensor, Tensor]:
+        batch_size = len(batch)
+        # pad tokids in the order of descending length
+        tokids_lens, orders = torch.tensor([len(tokids) for tokids, _ in batch]).sort(
+            dim=0, descending=True
+        )
+        max_tokids_len = tokids_lens[0]
+        pad_tokids = torch.zeros([batch_size, max_tokids_len], dtype=torch.int)
+        for i in range(batch_size):
+            pad_tokids[i, : len(batch[orders[i]][0])] = torch.tensor(
+                batch[orders[i]][0]
+            )
+        # pad mels in the same order
+        mel_dim = batch[0][1].shape[1]
+        mel_lens = torch.tensor(
+            [batch[orders[i]][1].shape[0] for i in range(batch_size)]
+        )
+        max_mel_len = torch.max(mel_lens)
+        pad_mel = torch.zeros([batch_size, max_mel_len, mel_dim])
+        pad_stp = torch.zeros([batch_size, max_mel_len])
+        for i in range(batch_size):
+            pad_mel[i, : mel_lens[i], :] = batch[orders[i]][1]
+            pad_stp[i, : mel_lens[i] - 1] = torch.exp(
+                torch.linspace(-20, -1, mel_lens[i] - 1)
+            )
+            pad_stp[i, mel_lens[i] - 1 :] = 1.0
+        # tokids_lens/mel_lens: [batch_size]
+        # pad_tokids: [batch_size, max_tokids_len]
+        # pad_mel: [batch_size, max_mel_len, mel_dim]
+        # pad_stp: [batch_size, max_mel_len]
+        return tokids_lens, pad_tokids, mel_lens, pad_mel, pad_stp
